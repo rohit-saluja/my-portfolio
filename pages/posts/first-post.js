6210196f5ea32507e59753f207b260023546ce2b
Rohit Salula <saluja730@gmail.com>
2023-12-28 15:54:31 +0530

    udated the branch ;

---
+++ pages/posts/first-post.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title> this is the first post</title>
+      </Head>
+      <div>
+        <h1>First post</h1>
+        <Link href="/"> Home page</Link>
+      </div>
+    </Layout>
+  );
+}
+
+
